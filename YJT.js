b00482780d24ad07c3057cd6d1de0b229eba526e
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2023-12-04 23:47:55 +0800

    Update YJT.js

--- YJT.js
+++ YJT.js
@@ -3,7 +3,7 @@
 易截图2：Rnik ZC
 
 [rewrite_local]
-^https?:\/\/(jietu6|1jietu)\.com\/apiv2\/user url script-response-body  https://raw.githubusercontent.com/Rnik666/Rnik-K/main/YjT.js
+^https?:\/\/(jietu6|1jietu)\.com\/apiv2\/user url script-response-body  https://raw.githubusercontent.com/Rnik666/RnikYJT/main/YJT.js
 
 [MITM]
 hostname = jietu6.com, 1jietu.com
