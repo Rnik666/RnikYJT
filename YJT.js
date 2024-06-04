78696978b21b0235ddf41d436b9babe123f23d2c
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-06-05 03:42:59 +0800

    更新 YJT.js

--- YJT.js
+++ YJT.js
@@ -1,6 +1,8 @@
 /*
 
-易截图2：Rnik ZC
+         易截图2
+         解锁永久VIP
+         @Rnik666
 
 [rewrite_local]
 ^https?:\/\/(jietu6|1jietu)\.com\/apiv2\/user url script-response-body  https://raw.githubusercontent.com/Rnik666/RnikYJT/main/YJT.js
@@ -13,5 +15,7 @@ var Rnik = JSON.parse($response.body);
 Rnik.info.group_id = "3";
 Rnik.info.vip = "1";
 Rnik.info.group = "老子是永久VIP";
+Rnik.inof.mobile = 18888888888;
 Rnik.info.nickname = "Rnik666🎖";
+Rnik.info.image = "https://sz-1300098290.cos.ap-beijing.myqcloud.com/app/avatar/b74cc83011b511ef99db5baca73f87d2.png";
 $done({ body: JSON.stringify(Rnik) });
