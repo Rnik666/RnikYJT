08195cd1c4d723a3dab8d21c1f895dfaf3893653
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-06-05 03:46:20 +0800

    更新 YJT.js

--- YJT.js
+++ YJT.js
@@ -15,7 +15,7 @@ var Rnik = JSON.parse($response.body);
 Rnik.info.group_id = "3";
 Rnik.info.vip = "1";
 Rnik.info.group = "老子是永久VIP";
-Rnik.inof.mobile = 18888888888;
+Rnik.inof.mobile = "18888888888";
 Rnik.info.nickname = "Rnik666🎖";
 Rnik.info.image = "https://sz-1300098290.cos.ap-beijing.myqcloud.com/app/avatar/b74cc83011b511ef99db5baca73f87d2.png";
 $done({ body: JSON.stringify(Rnik) });
